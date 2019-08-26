021090a057a31863f79a174b1d650f5b602321e2
gahao <603415477@qq.com>
2019-08-26 15:26:04 +0800

    配置中心修改

--- gateway-server/src/main/java/com/musketeers/gatewayservice/GatewayServiceApplication.java
+++ gateway-server/src/main/java/com/musketeers/gatewayservice/GatewayServiceApplication.java
@@ -1,15 +1,26 @@
 package com.musketeers.gatewayservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Value("${version}")
+	String version;
+	@RequestMapping(value = "/version")
+	public String hi(){
+		return version;
 	}
 
 }

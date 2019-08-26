c826d1bef5c8630775752be910c3a2db3c6fa703
gahao <603415477@qq.com>
2019-08-26 14:40:17 +0800

    配置中心提交

---
+++ config-service/src/main/java/com/musketeers/configservice/ConfigServiceApplication.java
@@ -0,0 +1,20 @@
+package com.musketeers.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@EnableEurekaClient
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
